4004347bc8762a89ddb89804835989698362307c
chauncy <hs1104@163.com>
2017-10-17 09:12:28 +0800

    user login

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), 
     # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'), 
     # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-    url(r'^$', views.user_login_get, name='user_login_get'),
-    url(r'^login/$', views.user_login_post, name='user_login_post'),
+    # url(r'^$', views.user_login_get, name='user_login_get'),
+    url(r'^profile/$', views.user_profile, name='user_profile'), 
+    # url(r'^login/$', views.user_login_post, name='user_login_post'),
+    # url(r'^logout/$', views.user_logout, name='user_logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def user_login_get(request):
     return render(request, 'user/login.html', {})
 
 
 def user_login_post(request):
-    print('get request!')
     username = request.POST['username']
     password = request.POST['password']
     print('get username : %s , get password : %s' % (username, password))
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponse('get login request')
+        return render(request, 'user/profile.html', {'user': user})
     else:
-        return HttpResponse('no such user exist!')
+        return render(request, 'user/profile.html', {'error_message': 'no such user exist!'})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'user/profile.html', {'user': request.user})
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('user:user_login_get'))
