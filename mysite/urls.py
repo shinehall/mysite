4c03c09d1d710871ad1eed17190d0da7abd312d0
chauncy <hs1104@163.com>
2017-10-23 22:04:02 +0800

    auth system

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('user.urls')), 
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='auth/login.html')), 
+    # url(r'^logout/$', auth_views.logout_then_login, name='logout_then_login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view()), 
+    url(r'^', include('django.contrib.auth.urls')), 
     url(r'^polls/', include('polls.urls')),
+    url(r'^user/', include('user.urls'))
 ]
+
+
+# ^login/$ [name='login']
+# ^logout/$ [name='logout']
+# ^password_change/$ [name='password_change']
+# ^password_change/done/$ [name='password_change_done']
+# ^password_reset/$ [name='password_reset']
+# ^password_reset/done/$ [name='password_reset_done']
+# ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
+# ^reset/done/$ [name='password_reset_complete']
